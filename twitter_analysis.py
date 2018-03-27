2922f7f7a253c5129661b3863e97dcc12addb7a2
Arnab Ray <arnabkeya04@gmail.com>
2018-03-27 22:39:21 +0530

    Update twitter_analysis.py

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key=	'yo8mYPmefJ82Lt5pqPz2JiteT'
-consumer_secret='e8L7dsV5qiQOSbkj3iY6XW25DLh5RG0JQWkkhUX3wHzBFAdu5a'
+consumer_key=	''
+consumer_secret=''
 
-access_token='4884998369-5dvP3ZYzrnBaa2DFBRT0RwSV8VUCz8oG3uJYOBq'
-access_token_secret='MECbKyA141YaTvQIQG0OJKBOL9WmHEenMdzImT8jN6nbr'
+access_token=''
+access_token_secret=''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
